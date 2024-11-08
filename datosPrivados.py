4cb210e76f3cd7b1c4e2b802fd603950737cacf7
Moises Jeter <moisesjeter@gmail.com>
2024-11-08 16:37:02 -0500

    subir sin datosPrivados.py

--- datosPrivados.py
+++
@@ -1,5 +0,0 @@
-apiKeyGemini="AIzaSyAnpiIg_zMV7U_LRgJ266dxFMzPGTiTcOQ"
-
-conexionColab="https://786e-34-82-11-255.ngrok-free.app"
-
-miLocal="C:/Users/MOISES NEGRETE/Documents/Proyectos/Eduai/EduAi/imagenesIA/"
